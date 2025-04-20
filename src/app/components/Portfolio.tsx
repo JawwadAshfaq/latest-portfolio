1da852044f3c0a3b120037393268805bbce4a7af
Jawwad Ashfaq <jawwadritz3@gmail.com>
2025-04-20 19:59:21 +0500

    Updated Portfolio.tsx file

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -11,13 +11,13 @@ import { motion } from 'framer-motion';
 // Ensure these paths and filenames exactly match what's in your public directory
 const portfolioItems = [
   { image: '/public/project1.jpg', alt: 'Project 1' },
-  { image: '/project2.jpg', alt: 'Project 2' },
-  { image: '/project3.jpg', alt: 'Project 3' },
-  { image: '/project4.jpg', alt: 'Project 4' },
-  { image: '/project1.jpg', alt: 'Project 1 Duplicate' },
-  { image: '/project2.jpg', alt: 'Project 2 Duplicate' },
-  { image: '/project3.jpg', alt: 'Project 3 Duplicate' },
-  { image: '/project4.jpg', alt: 'Project 4 Duplicate' },
+  { image: '/public/project2.jpg', alt: 'Project 2' },
+  { image: '/public/project3.jpg', alt: 'Project 3' },
+  { image: '/public/project4.jpg', alt: 'Project 4' },
+  { image: '/public/project1.jpg', alt: 'Project 1 Duplicate' },
+  { image: '/public/project2.jpg', alt: 'Project 2 Duplicate' },
+  { image: '/public/project3.jpg', alt: 'Project 3 Duplicate' },
+  { image: '/public/project4.jpg', alt: 'Project 4 Duplicate' },
 ];
 
 const PortfolioSlider = () => {
