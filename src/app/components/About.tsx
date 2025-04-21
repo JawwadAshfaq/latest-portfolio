3200be8e6b62269cd24d995556e6185f1d8d79d4
Jawwad Ashfaq <jawwadritz3@gmail.com>
2025-04-21 23:44:20 +0500

    Update About.tsx component

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -20,7 +20,7 @@ export default function HeroSection() {
 
           {/* Heading with Typewriter */}
           <h2 className="about-head text-3xl md:text-5xl font-bold pb-4 leading-snug">
-            Hi, I’m <span className="text-[#800080]">Jawwad Ashfaq</span>
+            Hi, I’m <span className="text-[#800080]">Jawad Ashfaq</span>
             <span className="text-white">, </span>
             <span className="block">
               <Typewriter
